cd250cb467e0d7f2c89fbab5423c6d944bb211b3
nguyen5562 <nknguyen112345@gmail.com>
2025-09-13 12:12:21 +0700

    deploy

--- Models/WebTinTucContext.cs
+++ Models/WebTinTucContext.cs
@@ -28,8 +28,10 @@ public partial class WebTinTucContext : DbContext
     public virtual DbSet<TrangThaiBaiViet> TrangThaiBaiViets { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=MYLAB\\NGUYEN;Initial Catalog=WebTinTuc;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    {
+        // Connection string được cấu hình trong Program.cs thông qua dependency injection
+        // Không cần cấu hình ở đây nữa
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
